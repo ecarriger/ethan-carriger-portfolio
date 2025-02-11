cb1e0d66e0933197ab011b05de4e5e9cc9998c76
Ethan Carriger <ethancarriger@gmail.com>
2025-02-11 14:06:15 -0500

    nav expand collapse functionaliity

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const navButton = document.querySelector('nav button');
+const navLinks = document.querySelectorAll('nav a');
 const nav = document.querySelector('nav');
+const overlay = document.getElementById('overlay');
+
+const expandNav = () => {
+    nav.classList.add('expanded');
+    overlay.classList.add('visible');
+};
+const collapseNav = () => {
+    nav.classList.remove('expanded');
+    overlay.classList.remove('visible');
+}
+
 navButton.addEventListener('click', () => {
     const menuExpanded = nav.classList.contains('expanded');
-    if(menuExpanded) {
-        nav.classList.remove('expanded');
-    }
-    else {
-        nav.classList.add('expanded');
-    }
+    menuExpanded ? collapseNav() : expandNav();
 });
+overlay.addEventListener('click', collapseNav);
+navLinks.forEach(link => link.addEventListener('click', collapseNav));
